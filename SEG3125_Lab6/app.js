a66bb01f302f044a2c883fe9c62f34ab39f6c551
Ariane Poserio <43153209+apose046@users.noreply.github.com>
2020-06-29 17:54:36 -0400

    Add files via upload

---
+++ SEG3125_Lab6/app.js
@@ -0,0 +1,24 @@
+// Entry point for the application
+
+// express application
+var express = require('express');
+// require the controller we make
+var surveyController = require('./surveyController');
+
+var app = express();
+
+// set up template engine
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static('./public'));
+
+// static file serving
+app.use(express.static('./public'));
+
+// fire function from surveyController
+surveyController(app);
+
+// listen to port
+app.listen(3000);
+console.log('listening port 3000');
---
+++ SEG3125_Lab6/public/assets/action.js
@@ -0,0 +1,21 @@
+// jQuery that will "listen" to the html niceSurvey.html
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+      
+       var item = $('form input');
+       console.log(item.serializeArray());
+
+      $.ajax({
+        type: 'POST',
+        url: '/index',
+        data: $(this).serializeArray(),
+        success: function(data){
+          // do something with the data via front-end framework
+          // Make the submit button red, disabled and saying Thank you
+          $("#bb").prop("disabled", "true");
+        }
+      });
+      return false;
+  });
+});
